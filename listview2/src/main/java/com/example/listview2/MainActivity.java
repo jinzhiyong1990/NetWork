26487b52a70e64cb56146d1e8dea2b7c3df247ab
jinzhiyong1990 <jinzhiyong1990@gmail.com@server.com>
2018-11-05 21:26:12 -0800

    Mac Version34

---
+++ listview2/src/main/java/com/example/listview2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.listview2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_app_list_view);
+
+
+
+
+    }
+}
