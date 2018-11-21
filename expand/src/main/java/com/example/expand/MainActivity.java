c01b13d0492f666d6ff8f58f53ca6cce8fc07e01
jinzhiyong1990 <jinzhiyong1990@gmail.com@server.com>
2018-11-20 16:22:52 -0800

    Mac 11/20/2018

---
+++ expand/src/main/java/com/example/expand/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.expand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//ExpandableListView:
+//可以扩展收缩的ListView
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
