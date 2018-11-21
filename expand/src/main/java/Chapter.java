9f800848f54574b45a17d59fece8b0a7bbfd47f2
Zhiyong Jin <jinzhiyong1990@gmail.com>
2018-11-20 23:54:30 -0800

    11/20/2018 windows3

---
+++ expand/src/main/java/Chapter.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 分组条目
+ */
+
+public class Chapter {
+    private int id;
+    private String name;
+
+    //一对多的关系Chapter a& ChapterItem
+    private List<ChapterItem> children = new ArrayList<>();
+
+    public Chapter(){
+
+    }
+
+    public Chapter(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void addChild(ChapterItem chapterItem){
+        chapterItem.setPid(getId());
+        children.add(chapterItem);
+
+    }
+
+    public void addChild(int cid, String name) {
+        ChapterItem chapterItem = new ChapterItem(cid, name);
+        chapterItem.setPid(getId());
+        children.add(chapterItem);
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ChapterItem> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<ChapterItem> children) {
+        this.children = children;
+    }
+}
--- expand/src/main/java/com/example/expand/MainActivity.java
+++ expand/src/main/java/com/example/expand/MainActivity.java
@@ -2,15 +2,82 @@ package com.example.expand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+
+import java.util.List;
 
 //ExpandableListView:
 //可以扩展收缩的ListView
 
 public class MainActivity extends AppCompatActivity {
+    private ExpandableListView expandableListView;
+    private BaseExpandableListAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        expandableListView = findViewById(R.id.expandableListView);
+        expandableListView.setAdapter(new BaseExpandableListAdapter() {
+            @Override
+            public int getGroupCount() {
+                return 0;
+            }
+
+            @Override
+            public int getChildrenCount(int groupPosition) {
+                return 0;
+            }
+
+            @Override
+            public Object getGroup(int groupPosition) {
+                return null;
+            }
+
+            @Override
+            public Object getChild(int groupPosition, int childPosition) {
+                return null;
+            }
+
+            @Override
+            public long getGroupId(int groupPosition) {
+                return 0;
+            }
+
+            @Override
+            public long getChildId(int groupPosition, int childPosition) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public boolean isChildSelectable(int groupPosition, int childPosition) {
+                return false;
+            }
+        });
+
     }
 }
